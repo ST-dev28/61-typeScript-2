5c7110d81b479aa68f4dd7ee0593addde6e9b009
SandraT <sndrbltr@gmail.com>
2021-10-06 15:40:03 +0000

    Solution

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * 1. Susikurkite TypeScript projektą
+ *
+ * 2. Parašykite programą, kuri padeda dirbti su trikampiais
+ *
+ * 3. Aprašykite klasę "Trikampis", kuri turėtu tris atributus
+ * simbolizuojančius visas tris trikampio kraštines (a, b, c).
+ *
+ * 4. Trikampio kraštinių reikšmės turi būti priskiriamos sukuriant
+ * objektą (konstruktoriuje).
+ *
+ * 5. Aprašykite metodą "spausdintiDuomenis()", kuris atspausdina
+ * visus trikampio duomenis (kraštines) į konsolę.
+ */
+var Trikampis = /** @class */ (function () {
+    function Trikampis(a, b, c) {
+        console.log("Konstruktorius iskviestas");
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    Trikampis.prototype.spausdintiDuomenis = function () {
+        console.log("Trikampio krastines yra: " + this.a + ", " + this.b + ", " + this.c + " cm.");
+        console.log("------------");
+    };
+    Trikampis.prototype.trikampioPerimetas = function () {
+        return this.a + this.b + this.c;
+    };
+    return Trikampis;
+}());
+var trikampis1 = new Trikampis(10, 10, 20);
+var trikampis2 = new Trikampis(10, 15, 30);
+trikampis1.spausdintiDuomenis();
+trikampis2.spausdintiDuomenis();
---
+++ index.ts
@@ -0,0 +1,42 @@
+/**
+ * 1. Susikurkite TypeScript projektą
+ *
+ * 2. Parašykite programą, kuri padeda dirbti su trikampiais
+ *
+ * 3. Aprašykite klasę "Trikampis", kuri turėtu tris atributus
+ * simbolizuojančius visas tris trikampio kraštines (a, b, c).
+ *
+ * 4. Trikampio kraštinių reikšmės turi būti priskiriamos sukuriant
+ * objektą (konstruktoriuje).
+ *
+ * 5. Aprašykite metodą "spausdintiDuomenis()", kuris atspausdina
+ * visus trikampio duomenis (kraštines) į konsolę.
+ */
+
+
+class Trikampis {
+a: number;   
+b: number;
+c: number;
+
+constructor(a: number, b: number, c: number) {
+console.log("Konstruktorius iskviestas");
+this.a = a;
+this.b = b;
+this.c = c;
+}
+spausdintiDuomenis(): void {
+        console.log(`Trikampio krastines yra: ${this.a}, ${this.b}, ${this.c} cm.`);
+        console.log("------------");
+    }
+
+    trikampioPerimetas(): number {
+        return this.a + this.b + this.c;
+    }
+}
+
+const trikampis1 = new Trikampis(10, 10, 20);
+const trikampis2 = new Trikampis(10, 15, 30);
+trikampis1.spausdintiDuomenis();
+trikampis2.spausdintiDuomenis();
+
